fe3229b4c57fc3f213835e302cef08e36598b4e0
Soran Raof <110354376+SoranRaof@users.noreply.github.com>
2023-09-04 13:33:34 +0100

    Refactor tests, added commands

--- Cypress/e2e/sauce-demo-login.cy.js
+++ Cypress/e2e/sauce-demo-login.cy.js
@@ -1,43 +1,32 @@
 describe("Login functionality", () => {
-  it("standard_user can log in with valid password", () => {
+  beforeEach(() => {
     cy.visit("https://www.saucedemo.com");
-    cy.get("[data-test=username]").type("standard_user");
-    cy.get("[data-test=password]").type("secret_sauce");
-    cy.get("[data-test=login-button]").click();
+  });
+
+  it("standard_user can log in with valid password", () => {
+    cy.login("standard_user", "secret_sauce");
     cy.url().should("include", "/inventory.html");
   });
 
   it("standard_user cannot log in with invalid password", () => {
-    cy.visit("https://www.saucedemo.com");
-    cy.get("[data-test=username]").type("standard_user");
-    cy.get("[data-test=password]").type("wrong_password");
-    cy.get("[data-test=login-button]").click();
-    cy.get("[data-test=error]").should("be.visible");
-    cy.get("[data-test=error]").should(
-      "have.text",
-      "Epic sadface: Username and password do not match any user in this service"
-    );
+    cy.login("standard_user", "wrong_sauce");
+    cy.errorMessageVisible();
+    cy.invalidPasswordErrorMessage();
   });
 
   it("locked_out_user cannot log in", () => {
-    cy.visit("https://www.saucedemo.com");
-    cy.get("[data-test=username]").type("locked_out_user");
-    cy.get("[data-test=password]").type("secret_sauce");
-    cy.get("[data-test=login-button]").click();
-    cy.get("[data-test=error]").should("be.visible");
-    cy.get("[data-test=error]").should(
-      "have.text",
-      "Epic sadface: Sorry, this user has been locked out."
-    );
+    cy.login("locked_out_user", "secret_sauce");
+    cy.errorMessageVisible();
+    cy.lockedOutUserErrorMessage();
   });
 
   it("error message when logging in with no username or password", () => {
-    cy.visit("https://www.saucedemo.com");
-    cy.get("[data-test=login-button]").click();
-    cy.get("[data-test=error]").should("be.visible");
-    cy.get("[data-test=error]").should(
-      "have.text",
-      "Epic sadface: Username is required"
-    );
+    cy.noCredentials();
+  });
+
+  it("logout button logs out user", () => {
+    cy.login("standard_user", "secret_sauce");
+    cy.url().should("include", "/inventory.html");
+    cy.logout();
   });
 });
--- Cypress/support/commands.js
+++ Cypress/support/commands.js
@@ -1,25 +1,45 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const loginButton = "[data-test=login-button]";
+const logoutButton = "[data-test=logout_side_bar_link]";
+const menuButton = "[data-test=react-burger-menu-btn]";
+const usernameInput = "[data-test=username]";
+const passwordInput = "[data-test=password]";
+const errorElement = "[data-test=error]";
+
+Cypress.Commands.add("login", (username, password) => {
+  cy.get(usernameInput).type(username);
+  cy.get(passwordInput).type(password);
+  cy.get(loginButton).click();
+});
+
+Cypress.Commands.add("errorMessageVisible", () => {
+  cy.get(errorElement).should("be.visible");
+});
+
+Cypress.Commands.add("invalidPasswordErrorMessage", () => {
+  cy.get(errorElement).should(
+    "have.text",
+    "Epic sadface: Username and password do not match any user in this service"
+  );
+});
+
+Cypress.Commands.add("lockedOutUserErrorMessage", () => {
+  cy.get(errorElement).should(
+    "have.text",
+    "Epic sadface: Sorry, this user has been locked out."
+  );
+});
+
+Cypress.Commands.add("noCredentials", () => {
+  cy.get(loginButton).click();
+  cy.get(errorElement).should("be.visible");
+  cy.get(errorElement).should(
+    "have.text",
+    "Epic sadface: Username is required"
+  );
+});
+
+Cypress.Commands.add("logout", () => {
+  cy.get(menuButton).click();
+  cy.get(logoutButton).click();
+  cy.url().should("include", "sauce-demo.com/");
+});
